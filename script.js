202e5420dbac2f91dd15715b9e957bfd220d1c93
shivamm-55 <shivammishra76207@gmail.com>
2023-07-17 14:05:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow,i)=>{
+    const itemNum = movieLists[i].querySelectorAll("img").length;
+    let clickCounter = 0;
+
+    arrow.addEventListener("click", () => {
+        const ratio = Math.floor(window.innerWidth/270);
+        clickCounter++;
+        if (itemNum - (4 + clickCounter) + (4 - ratio) >= 0) {
+        movieLists[i].style.transform = `translateX(${
+            movieLists[i].computedStyleMap().get("transform")[0].x.value -310}px)`;
+        }
+        else{
+            movieLists[i].style.transform = "translateX(0)"
+            clickCounter = 0;
+        }
+    });
+
+    //  console.log(Math.floor(window.innerWidth/270));
+});
+
+// Toggle
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle,.toggle-ball");
+
+ball.addEventListener("click", ()=>{
+    items.forEach(item=>{
+        item.classList.toggle("active")
+    });
+});
